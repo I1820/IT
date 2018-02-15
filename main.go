d60b8a3fd1fb10b4f886a427973640a4e4fd2379
Parham Alvani <parham.alvani@gmail.com>
2018-02-15 12:19:00 -0500

    Adds configuration and store jwt flag.

--- main.go
+++ main.go
@@ -12,22 +12,40 @@ package main
 
 import (
 	"crypto/tls"
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 
+	"github.com/jinzhu/configor"
 	log "github.com/sirupsen/logrus"
 )
+
+// Config represents main configuration
+var Config = struct {
+	BackBack struct {
+		BaseURL string `default:"https://backback.ceit.aut.ac.ir/api/" env:"backback_base_url"`
+		Version string `default:"v1" env:"backback_version"`
+	}
+}{}
+
+// JWT Token
+var jwtToken token
 
 func main() {
 	// Disable https certificate validation
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 
+	// Load configuration
+	if err := configor.Load(&Config, "config.yml"); err != nil {
+		panic(err)
+	}
+
 	login()
 }
 
 func login() {
-	resp, err := http.PostForm("https://backback.ceit.aut.ac.ir/api/v1/login", url.Values{"email": {"sepehr.sabour@gmail.com"}, "password": {"1234567"}})
+	resp, err := http.PostForm(Config.BackBack.BaseURL+Config.BackBack.Version+"/login", url.Values{"email": {"sepehr.sabour@gmail.com"}, "password": {"1234567"}})
 	if err != nil {
 		log.WithFields(log.Fields{
 			"Phase": "login",
@@ -53,9 +71,24 @@ func login() {
 		}).Fatalf("Body: %s", err)
 	}
 
+	var response struct {
+		Code   int
+		Result struct {
+			User  user
+			Token token
+		}
+	}
+	if err := json.Unmarshal(data, &response); err != nil {
+		log.WithFields(log.Fields{
+			"Phase": "login",
+		}).Fatalf("JSON Unmarshal: %s", err)
+	}
+
 	log.WithFields(log.Fields{
 		"Phase": "login",
-	}).Infoln(string(data))
+	}).Infoln(response)
+
+	jwtToken = response.Result.Token
 }
 
 func createProject() {
