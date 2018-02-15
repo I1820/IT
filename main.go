ade5e5d912443c397beb3ef2b9c6135ddd683db5
Parham Alvani <parham.alvani@gmail.com>
2018-02-15 06:31:45 -0500

    Adds steps of test.

---
+++ main.go
@@ -0,0 +1,26 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 15-02-2018
+ * |
+ * | File Name:     main.go
+ * +===============================================
+ */
+
+package main
+
+func main() {
+}
+
+func createProject() {
+}
+
+func createThingProfile() {
+}
+
+func createThing() {
+}
+
+func activateThing() {
+}
