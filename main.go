66b2b59a938f52778f62ed172d168410f87c504d
Parham Alvani <parham.alvani@gmail.com>
2018-02-15 09:19:05 -0500

    Can we login :joy:

--- main.go
+++ main.go
@@ -10,7 +10,23 @@
 
 package main
 
+import (
+	"net/http"
+	"net/url"
+
+	log "github.com/sirupsen/logrus"
+)
+
 func main() {
+	login()
+}
+
+func login() {
+	resp, err := http.PostForm("https://backback.ceit.aut.ac.ir/api/v1/login", url.Values{"email": {"sepehr.sabour@gmail.com"}, "password": {"1234567"}})
+	if err != nil {
+		log.Fatalf("Login: %s", err)
+	}
+	log.Infoln(resp)
 }
 
 func createProject() {
