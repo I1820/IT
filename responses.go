9cd3f3bd4ee49abcbd32959e0f5188ac0d37f8e9
Parham Alvani <parham.alvani@gmail.com>
2018-02-15 12:19:55 -0500

    Adds response structures.

---
+++ responses.go
@@ -0,0 +1,17 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 15-02-2018
+ * |
+ * | File Name:     responses.go
+ * +===============================================
+ */
+
+package main
+
+type user struct {
+	Token string
+}
+
+type token string
